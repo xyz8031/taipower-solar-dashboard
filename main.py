477424412ee428f70415a822a49053286df4b688
jwz823 <jiawei@weatherrisk.com>
2021-07-28 10:39:29 +0800

    add 2 plots

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import pandas as pd
 from datetime import timedelta
 import s3fs
 
-# fs = s3fs.S3FileSystem(anon=False, key=st.secrets["key"], secret=st.secrets["secret"])
-
 st.title('全台灣太陽能發電量預報（修正後）')
 
 password = st.text_input("Enter a password", type="password")
 
-    
 
-data = pd.read_csv("s3://taipower-green-energy-data/aggregations/calibrated-aggregation.csv").iloc[:48*6].set_index('PeriodEnd')
-data.index = pd.to_datetime(data.index) + timedelta(hours = 16)
+data = pd.read_csv("s3://taipower-green-energy-data/aggregations/calibrated-aggregation.csv")
+data.PeriodEnd = pd.to_datetime(data.PeriodEnd) + timedelta(hours = 16)
 
 # plot
 
+st.title('未來168小時')
+
 if password == 'wr-taipower-temp-dashboard':
-    st.line_chart(data)
+    st.line_chart(data.set_index('PeriodEnd'))
+else:
+    st.write('密碼錯誤！')
+    
+st.title('未來48小時')
+
+if password == 'wr-taipower-temp-dashboard':
+    st.line_chart(data.iloc[:48*6].set_index('PeriodEnd'))
 else:
     st.write('密碼錯誤！')
     
