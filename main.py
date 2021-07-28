6d68a7f02da8b9e5e878dc52a1c079e19402cc4c
jwz823 <jiawei@weatherrisk.com>
2021-07-28 10:01:33 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from datetime import timedelta
+import s3fs
+
+st.title('全台灣太陽能發電量預報（修正後）')
+
+password = st.text_input("Enter a password", type="password")
+
+    
+
+data = pd.read_csv("s3://taipower-green-energy-data/aggregations/calibrated-aggregation.csv").iloc[:48*6].set_index('PeriodEnd')
+data.index = pd.to_datetime(data.index) + timedelta(hours = 8)
+
+# plot
+
+if password == 'wr-taipower-temp-dashboard':
+    st.line_chart(data)
+else:
+    st.write('密碼錯誤！')
